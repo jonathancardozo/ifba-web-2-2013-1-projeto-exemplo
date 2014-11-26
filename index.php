e234f32469eab65ec802f230408cd2629364bf6a
jonathancardozo <jonathanmoreirac@gmail.com>
2014-11-26 20:16:17 -0300

    Implementada busca no index.

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php require_once "header.inc.php"; ?>
 <?php
-$sql = "SELECT * FROM pessoas";
-$resultado = $conn->query($sql);
+if(isset($_GET['q'])){
+	$q = ($_GET['q'])."%";
+	
+	$sql = "SELECT * FROM pessoas WHERE nome LIKE ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param('s',$q);
+	$stmt->execute();
+	$resultado = $stmt->get_result();
+	
+	
+} else {
+	$sql = "SELECT * FROM pessoas;";
+	$resultado = $conn->query($sql);
+}
+
 $rows = $resultado->fetch_all(MYSQLI_ASSOC);
 $resultado->free();
 ?>
 <h3>Lista de Pessoas</h3>
+
+<form method="get">
+<label for="q">Buscar: </label>
+<input type="text" id="q" name="q" />
+<input type="submit" value="Buscar" />
+</form>
+
 
 <table id="lista">
 <thead>
